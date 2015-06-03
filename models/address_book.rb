75d8a029e00c02780e666680e304b70da9d1a75b
mBabbitt <meganbabbitt23@gmail.com>
2015-06-03 11:49:04 -0700

    Assignment 21 completed

---
+++ models/address_book.rb
@@ -0,0 +1,28 @@
+require_relative "entry.rb"
+
+class AddressBook
+    attr_accessor :entries
+    
+    def initialize
+        @entries = []
+    end
+    
+    def add_entry(name, phone, email)
+        # #7
+        index = 0
+        @entries.each do |entry|
+            # #8
+            if name < entry.name
+                break
+            end
+            index += 1
+        end
+        # #9
+        @entries.insert(index, Entry.new(name, phone, email))
+    end
+    
+    def remove_entry(entry)
+        @entries -= [entry]
+    end
+    
+end

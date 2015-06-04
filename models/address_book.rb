392db5c26d584ae2b20f9ba7389a2954fd5e6ff3
mBabbitt <meganbabbitt23@gmail.com>
2015-06-04 11:55:07 -0700

    checkpoint 23 with errors

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,28 +1,33 @@
-require_relative "entry.rb"
+ require_relative "entry"
+ require "csv"
 
-class AddressBook
-    attr_accessor :entries
-    
-    def initialize
-        @entries = []
-    end
-    
-    def add_entry(name, phone, email)
-        # #7
-        index = 0
-        @entries.each do |entry|
-            # #8
-            if name < entry.name
-                break
-            end
-            index += 1
-        end
-        # #9
-        @entries.insert(index, Entry.new(name, phone, email))
-    end
-    
-    def remove_entry(entry)
-        #@entries -= [entry]
-        @entries.delete(entry)
-    end
+ class AddressBook
+   attr_accessor :entries
+
+   def initialize
+     @entries = []
+   end   
+
+   def add_entry(name, phone, email)
+     index = 0
+     @entries.each do |entry|
+     if name < entry.name
+       break
+     end
+     index += 1
+   end
+
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true)
+ 
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+ 
+     return csv.count
+  
+   end
+ end
 end

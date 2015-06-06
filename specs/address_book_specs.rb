d16b20baccfd5cba56e492fed830b09c66eb6f5f
mBabbitt <meganbabbitt23@gmail.com>
2015-06-06 11:00:14 -0700

    checkpoint-24 completed

--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -23,7 +23,6 @@ end
 context ".add_entry" do
   it "adds only one entry to the address book" do
   book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-            
   expect(book.entries.size).eql? 1
   end
         
@@ -100,7 +99,50 @@ end
     check_entry(entry_three, "Ana", "555-555-5555", "ana@gmail.com") 
   end
 end
-end
+
+  describe ".binary_search" do
+    it "searches AddressBook for a non-existant entry" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Dan")
+      expect(entry).to be_nil
+    end
+    it "searches AddresBook for Bill" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Bill")
+      expect entry.instance_of?(Entry)
+      check_entry(entry, "Bill", "555-555-5555", "bill@blocmail.com")
+    end
+    it "searches AddresBook for Bob" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Bob")
+      expect entry.instance_of?(Entry)
+      check_entry(entry, "Bob", "555-555-5555", "bob@blocmail.com")
+    end
+    it "searches AddresBook for Joe" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Joe")
+      expect entry.instance_of?(Entry)
+      check_entry(entry, "Joe", "555-555-5555", "joe@blocmail.com")
+    end
+    it "searches AddresBook for Sally" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Sally")
+      expect entry.instance_of?(Entry)
+      check_entry(entry, "Sally", "555-555-5555", "sally@blocmail.com")
+    end
+    it "searches AddresBook for Sussie" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Sussie")
+      expect entry.instance_of?(Entry)
+      check_entry(entry, "Sussie", "555-555-5555", "sussie@blocmail.com")
+    end
+    it "searches AddressBook for Billy" do
+      book.import_from_csv("entries.csv")
+      entry = book.binary_search("Billy")
+      expect(entry).to be_nil
+    end
+  end
+ end
 
 
 
